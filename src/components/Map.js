7eabad7f188011c7896b1964079ad2d5d0b4cb0f
SalwaRayan <90609792+SalwaRayan@users.noreply.github.com>
2021-11-22 21:13:54 +0100

    done hover

--- src/components/Map.js
+++ src/components/Map.js
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import { MapContext } from "../contexts/Map";
 import GoogleMapReact from "google-map-react";
 import styled from 'styled-components'
+import Marker from "./Marker";
 
 const MapContainer = styled.div`
   height: 100vh;
@@ -9,22 +10,41 @@ const MapContainer = styled.div`
 `
 
 const Map = () => {
-  const { location } = useContext(MapContext);
+  const { location, bars } = useContext(MapContext);
+  // const location = useContext(MapContext);
 
   if (!location) {
     return <p>Chargement...</p>;
   }
 
   console.log(location)
+  console.log(bars)
 
   return (
     <MapContainer>
       <GoogleMapReact
         bootstrapURLKeys={{ key: "" }}
         defaultCenter={location}
-        defaultZoom={11}
+        defaultZoom={14}
       >
 
+        <Marker 
+          lat={location.lat}
+          lng={location.lng}
+          marker='my-position'
+        />
+
+        {bars.map((bar, index) => (
+          <Marker 
+          key={index}
+          lat={bar.latitude}
+          lng={bar.longitude}
+          marker='local-position'
+          address={bar.address}
+          name={bar.name}
+          price={bar.price}
+        />
+        ))}
       </GoogleMapReact>
     </MapContainer>
   );
---
+++ src/components/Marker.js
@@ -0,0 +1,53 @@
+import styled from "styled-components"
+import { FaMapMarkerAlt } from 'react-icons/fa'
+import { useState } from "react"
+
+const MarkerContainer = styled.div`
+  width: 40px;
+  height: 40px;
+  position: relative;
+`
+
+const Marker = props => {
+  const [ display, setDisplay ] = useState(false)
+
+  // console.log(props.bar)
+  return (
+    <MarkerContainer>
+      {props.marker === 'local-position' && 
+        <div 
+          style={{
+            display: `${!display ? 'none' : 'initial'}`,
+            position: 'absolute',
+            bottom: '0',
+            left: '50%',
+            width: '150px',
+            background: 'white',
+            borderRadius: '10px',
+            // lineHeight: '5px',
+            padding: '5px',
+            zIndex: 10,
+          }}
+        >
+          <p>{props.name}</p>
+          <p>{props.address}</p>
+          <p>{props.price}</p>
+        </div>
+      }
+      <FaMapMarkerAlt 
+        style={{
+          width: '40px',
+          height: '40px',
+          color: `${props.marker === " my-position" ? 'red' : !display ? 'gray' : 'blue'}`,
+          position: 'absolute',
+          bottom: '100%',
+          left: '-20px',
+        }}  
+        onMouseEnter={() => setDisplay(true)}
+        onMouseLeave={() => setDisplay(false)}
+      />
+    </MarkerContainer>
+  )
+}
+
+export default Marker
